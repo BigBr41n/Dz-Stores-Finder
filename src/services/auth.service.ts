f78e3b85ee3368ef873762b1bb1e706ec6c45e46
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-12 21:34:08 +0200

    login auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,12 @@
 import User from "../models/user.model";
 import crypto from "crypto";
-import { USER } from "../utils/types";
+import bcrypt from "bcryptjs";
+import { SIGNUP, LOGIN } from "../utils/types";
 import { sendActivationEmail } from "../utils/mailer";
+import { ApiError } from "../utils/apiError";
+import { signJwt, signRefreshToken } from "../utils/jwt.utils";
 
-export const signUp = async (userData: USER): Promise<USER> => {
+export const signUp = async (userData: SIGNUP): Promise<SIGNUP> => {
   const activationToken = crypto.randomBytes(32).toString("hex");
 
   const activeExpires = Date.now() + 1000 * 60 * 60; // 1h
@@ -18,4 +21,25 @@ export const signUp = async (userData: USER): Promise<USER> => {
   //send activation email
   sendActivationEmail(userData.email, userData.password, activationToken);
   return newUser;
+};
+
+export const login = async (userData: LOGIN): Promise<LOGIN> => {
+  const user = await User.findOne({ email: userData.email });
+  if (!user) {
+    throw new ApiError("User not found!", 404);
+  }
+
+  const isMatch = await bcrypt.compare(userData.password, user.password);
+  if (!isMatch) {
+    throw new ApiError("Invalid credentials!", 401);
+  }
+
+  const accessToken = signJwt({ id: user._id.toString() });
+  const refreshToken = signRefreshToken({ id: user._id.toString() });
+
+  return {
+    ...userData,
+    accessToken,
+    refreshToken,
+  };
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,12 @@
-export interface USER {
+export interface SIGNUP {
   name: string;
   email: string;
   password: string;
+}
+
+export interface LOGIN {
+  email: string;
+  password: string;
+  accessToken: string | null;
+  refreshToken: string | null;
 }
