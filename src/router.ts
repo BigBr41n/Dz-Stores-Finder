86c6ed1a35bb1c910f2419079d384c3567772ee3
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-13 17:16:30 +0200

    routes.ts router to auth routes

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,17 @@
 import { Express } from "express";
+import authRoutes from "./routes/auth.routes"
+
+
+
+
 function routes(app: Express) {
   app.get("/api/v1/healthCheck", (req, res) =>
     res.status(200).json("Server is On")
   );
+
+
+
+  //auth routes 
+  app.use('/api/v1/auth' , authRoutes)
 }
 export default routes;
