2209cb2847a28aa8f32381cd8bef1ec200e38078
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-17 19:36:39 +0200

    store routes

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -1,22 +1,37 @@
-/* 
 import express from 'express';
+import {
+  addNewStoreController,
+  updateStoreController,
+  deleteStoreController,
+  getAllStoresController,
+  getStoreByIdController,
+  getStoreByNameController,
+  addStoreReviewController,
+  filterStoresController,
+  searchStoresController,
+  uploadStoreImageController,
+} from '../controllers/store.controller';
+
 const router = express.Router();
 
 
+//NEXT : add authentication & authorization middleware 
+
+
 router
-    .post('/add-new-store' , addNewStoreController)
-    .put('/update-store/:id' , updateStoreController)
-    .delete('/delete-store/:id' , deleteStoreController)
-    .get('/get-all-stores' , getAllStoresController)
-    .get('/get-store-byId/:id' , getStoreByIdController)
-    
-    //.post('/upload-store-image', uploadStoreImageController);
+  .post('/stores', addNewStoreController) // endpoint to add a new store
+  .put('/stores/:id', updateStoreController) // endpoint to update a store by ID
+  .delete('/stores/:id', deleteStoreController) // endpoint to delete a store by ID
+  .get('/stores', getAllStoresController) // endpoint to get all stores
+  .get('/stores/:id', getStoreByIdController) // endpoint to get a store by ID
 
-    .post('/add-store-rating/:id', addStoreReviewController);
-    .delete('/delete-store-rating/:id', addStoreReviewController);
+  .post('/stores/:storeId/images', uploadStoreImageController) // endpoint to upload a store image by store ID
 
+  .post('/stores/:storeId/reviews', addStoreReviewController) // endpoint to add a review for a store by store ID
+  //.delete('/stores/:storeId/reviews/:reviewId', deleteStoreReviewController); // Example of endpoint to delete a store review by review ID
 
-    .get('/filter-stores-by-wilaya', filterStoresController); //with query
-    .get('/search-stores', searchStoresController);  //with query
-    .get('/get-store-byName', getStoreByNameController);  //with query
-*/
+  .get('/stores/filter-by-wilaya', filterStoresController) // endpoint to filter stores by wilaya (with query parameter)
+  .get('/stores/search', searchStoresController) // endpoint to search stores (with query parameter)
+  .get('/stores/search-by-name', getStoreByNameController); // endpoint to search stores by name (with query parameter)
+
+export default router;
