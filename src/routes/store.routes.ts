6e998c9b6cd338bc2a6f765ed38fdcaa2626c844
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-18 23:13:53 +0200

    store routes data validation

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -16,6 +16,9 @@ import {
 import {protect} from '../middleware/checkAuth';
 import {allowedTo} from '../middleware/authorization'
 
+import validate from '../middleware/validator';
+import * as schema from "../resources-schema/store.schema"
+
 const router = express.Router();
 
 
@@ -24,10 +27,10 @@ const router = express.Router();
 
 router
     .get("/" , getAllStoresController)
-    .get('/:id', getStoreByIdController) //endpoint to get a store by Id
-    .get('/filter-by-wilaya', filterStoresController) // endpoint to filter stores by wilaya (with query parameter)
-    .get('/search', searchStoresController) // endpoint to search stores (with query parameter)
-    .get('/search-by-name', getStoreByNameController); // endpoint to search stores by name (with query parameter)
+    .get('/:id', validate(schema.getStoreByIdSchema), getStoreByIdController) //endpoint to get a store by Id
+    .get('/filter-by-wilaya', validate(schema.filterStoresSchema),filterStoresController) // endpoint to filter stores by wilaya (with query parameter)
+    .get('/search',validate(schema.searchStoresSchema) ,searchStoresController) // endpoint to search stores (with query parameter)
+    .get('/search-by-name',validate(schema.getStoreByNameSchema), getStoreByNameController); // endpoint to search stores by name (with query parameter)
   
 
 
@@ -40,11 +43,11 @@ router.use(protect , allowedTo("user"));
 
 //protected routes : 
 router
-  .post("/" , addNewStoreController)
-  .put('/:id', updateStoreController) // endpoint to update a store by ID
-  .delete('/:id', deleteStoreController) // endpoint to delete a store by ID
+  .post("/" , validate(schema.StoreSchema),  addNewStoreController)
+  .put('/:id', validate(schema.UpdateStoreSchema) , updateStoreController) // endpoint to update a store by ID
+  .delete('/:id',validate(schema.deleteStoreSchema) , deleteStoreController) // endpoint to delete a store by ID
 
-  .post('/:storeId/images', uploadStoreImageController) // endpoint to upload a store image by store ID
+  .post('/:storeId/images',  uploadStoreImageController) // endpoint to upload a store image by store ID
 
   .post('/:storeId/reviews', addStoreReviewController) // endpoint to add a review for a store by store ID
   //.delete('/stores/:storeId/reviews/:reviewId', deleteStoreReviewController); // Example of endpoint to delete a store review by review ID
