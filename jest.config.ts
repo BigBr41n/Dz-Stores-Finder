52ebf1dcbe0da4d5de4b58470213a56779432c03
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-22 18:42:19 +0200

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,46 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  // Automatically clear mock calls, instances, contexts, and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ["json", "text", "lcov", "clover"],
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+
+  // Automatically reset mock state before every test
+  resetMocks: true,
+
+  // Automatically restore mock state and implementation before every test
+  restoreMocks: true,
+};
+
+export default config;
